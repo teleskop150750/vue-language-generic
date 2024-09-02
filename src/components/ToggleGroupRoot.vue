20dba0f4e378b5f32665c01f8826b49ec2e80f04
teleskop150750 <teleskop150750@gmail.com>
2024-09-02 08:56:29 +0300

    example

--- src/components/ToggleGroupRoot.vue
+++ src/components/ToggleGroupRoot.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts" generic="T extends ToggleGroupType">
 import type { ToggleGroupProps, ToggleGroupType } from './ToggleGroupRoot';
 
-
 defineProps<ToggleGroupProps<T>>()
-
-const isFoo = false
-
 </script>
 
 <template>
